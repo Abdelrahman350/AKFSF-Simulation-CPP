4a277f002649babb6d8f142113d2e88ea2444a24
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2022-08-23 01:40:32 +0200

    implement prediction

--- src/kalmanfilter_lkf_student.cpp
+++ src/kalmanfilter_lkf_student.cpp
@@ -13,8 +13,8 @@
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
 constexpr bool INIT_ON_FIRST_PREDICTION = true;
 constexpr double INIT_POS_STD = 0;
-constexpr double INIT_VEL_STD = 0;
-constexpr double ACCEL_STD = 0;
+constexpr double INIT_VEL_STD = 15;
+constexpr double ACCEL_STD = 0.1;
 constexpr double GPS_POS_STD = 3.0;
 // -------------------------------------------------- //
 
@@ -35,6 +35,12 @@ void KalmanFilter::predictionStep(double dt)
 
             // Assume the initial position is (X,Y) = (0,0) m
             // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
+            state << 0, 0, 5*cos(M_PI_4), 5*sin(M_PI_4);
+
+            cov(0, 0) = INIT_POS_STD * INIT_POS_STD;
+            cov(1, 1) = INIT_POS_STD * INIT_POS_STD;
+            cov(2, 2) = INIT_VEL_STD * INIT_VEL_STD;
+            cov(3, 3) = INIT_VEL_STD * INIT_VEL_STD;
 
             setState(state);
             setCovariance(cov);
@@ -51,8 +57,26 @@ void KalmanFilter::predictionStep(double dt)
         // Hint: You can use the constants: ACCEL_STD
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE
+        Matrix4d F = Matrix4d::Zero();;
+        F(0, 0) = 1;
+        F(0, 2) = dt;
+        F(1, 1) = 1;
+        F(1, 3) = dt;
+        F(2, 2) = 1;
+        F(3, 3) = 1;
+        
+        Matrix2d Q = Matrix2d::Zero();
+        Q(0, 0) = ACCEL_STD * ACCEL_STD;
+        Q(1, 1) = ACCEL_STD * ACCEL_STD;
 
+        MatrixXd L = MatrixXd(4, 2);
+        L(0, 0) = 0.5*dt*dt;
+        L(1, 1) = 0.5*dt*dt;
+        L(2, 0) = dt;
+        L(3, 1) = dt;
 
+        state = L * state;
+        cov = F * cov * F.transpose() + L * Q * L.transpose();
         // ----------------------------------------------------------------------- //
 
         setState(state);
